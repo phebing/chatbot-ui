9156c17d468c8ecf6c91e5b004fa1bccd5dd5cab
phebing <philipp.hebing@gmx.de>
2023-12-19 15:35:21 +0100

    Use local models

--- types/openai.ts
+++ types/openai.ts
@@ -8,38 +8,25 @@ export interface OpenAIModel {
 }
 
 export enum OpenAIModelID {
-  GPT_3_5 = 'gpt-3.5-turbo',
-  GPT_3_5_AZ = 'gpt-35-turbo',
-  GPT_4 = 'gpt-4',
-  GPT_4_32K = 'gpt-4-32k',
+  MISTRAL = 'mistral',
+  CODELLAMA = 'code-llama',
 }
 
 // in case the `DEFAULT_MODEL` environment variable is not set or set to an unsupported model
-export const fallbackModelID = OpenAIModelID.GPT_3_5;
+export const fallbackModelID = OpenAIModelID.MISTRAL;
 
 export const OpenAIModels: Record<OpenAIModelID, OpenAIModel> = {
-  [OpenAIModelID.GPT_3_5]: {
-    id: OpenAIModelID.GPT_3_5,
-    name: 'GPT-3.5',
-    maxLength: 12000,
-    tokenLimit: 4000,
+
+  [OpenAIModelID.MISTRAL]: {
+    id: OpenAIModelID.MISTRAL,
+    name: 'mistral',
+    maxLength: 12000, // TODO: test
+    tokenLimit: 4000, // TODO: test
   },
-  [OpenAIModelID.GPT_3_5_AZ]: {
-    id: OpenAIModelID.GPT_3_5_AZ,
-    name: 'GPT-3.5',
-    maxLength: 12000,
-    tokenLimit: 4000,
-  },
-  [OpenAIModelID.GPT_4]: {
-    id: OpenAIModelID.GPT_4,
-    name: 'GPT-4',
-    maxLength: 24000,
-    tokenLimit: 8000,
-  },
-  [OpenAIModelID.GPT_4_32K]: {
-    id: OpenAIModelID.GPT_4_32K,
-    name: 'GPT-4-32K',
-    maxLength: 96000,
-    tokenLimit: 32000,
+  [OpenAIModelID.CODELLAMA]: {
+    id: OpenAIModelID.CODELLAMA,
+    name: 'code-llama',
+    maxLength: 12000, // TODO: test
+    tokenLimit: 4000, // TODO: test
   },
 };
